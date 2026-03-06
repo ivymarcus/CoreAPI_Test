14ef2ecf4dd01d1ffd66cfedf1860bd974d4cc5a
art <art031565@gmail.com>
2026-03-06 22:21:48 +0800

    建立相關資訊model controller

---
+++ CoreAPI_Test/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreAPI_Test.Models
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public string? ProductName { get; set; }
+        public string? Category { get; set; }
+        public int Price { get; set; }
+    }
+}
--- CoreAPI_Test/obj/Debug/net8.0/CoreAPI_Test.AssemblyInfo.cs
+++ CoreAPI_Test/obj/Debug/net8.0/CoreAPI_Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreAPI_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e29fe9bed8acb08167333c80c85c7085aa4c3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreAPI_Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreAPI_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
