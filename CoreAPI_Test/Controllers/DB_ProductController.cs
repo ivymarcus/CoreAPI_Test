10e2102d58eda8c31e57a5803e684d5feff623f0
art <art031565@gmail.com>
2026-03-07 12:07:47 +0800

    完成連結 DB 並完成get相關資訊

---
+++ CoreAPI_Test/Controllers/DB_ProductController.cs
@@ -0,0 +1,37 @@
+﻿using CoreAPI_Test.Models;
+using CoreAPI_Test.Repostiroy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAPI_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DB_ProductController : ControllerBase
+    {
+        private readonly DB_ProductRepository _dbResp;
+        public DB_ProductController(IConfiguration config)
+        {
+            var conn = config.GetConnectionString("TestDB");
+            _dbResp = new DB_ProductRepository(conn);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DB_ProductModel>>> GetAllProduct()
+        {
+            IEnumerable<DB_ProductModel> model = await _dbResp.GetAllProduct();
+            return Ok(model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DB_ProductModel>> GetProductBtId(int id)
+        {
+            var model = await _dbResp.GetProductById(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Ok(model);
+        }
+    }
+}
---
+++ CoreAPI_Test/Repostiroy/DB_ProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using System.Data;
+using CoreAPI_Test.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace CoreAPI_Test.Repostiroy
+{
+    public class DB_ProductRepository
+    {
+        private readonly string _connectionString;
+        public DB_ProductRepository(string conn)
+        {
+            _connectionString = conn;
+        }
+
+        // Get All
+        public async Task<IEnumerable<DB_ProductModel>> GetAllProduct()
+        {
+            using IDbConnection dbConnection = new SqlConnection( _connectionString );
+            dbConnection.Open();
+            string sql = "select * from Product";
+            return await dbConnection.QueryAsync<DB_ProductModel>(sql);
+        }
+
+        // Get ByID
+        public async Task<DB_ProductModel?> GetProductById(int id)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+            string sql = "select * from Product where ProductID = @Id";
+            return await dbConnection.QueryFirstOrDefaultAsync<DB_ProductModel>(sql, new { Id = id });
+        }
+
+
+        // Post
+        public async Task<int> InsertProduct(DB_ProductModel model)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+            string sql = @"insert into Product (ProductName, UnitPrice, Discount, CategoryID, Explain)
+                           values(@ProductName, @UnitPrice, @Discount, @CategoryID, @Explain)";
+            return await dbConnection.ExecuteAsync(sql, model); // 回傳成功 更新/刪除 的紀錄條數
+        }
+
+
+        // Put
+        public async Task<int> UpdateProduct(DB_ProductModel model)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+            string sql = @"update Product
+                           set ProductName = @ProductName,
+                            UnitPrice = @UnitPrice,
+                            Explain = @Explain
+                           where ProductID = @ProductID";
+
+            return await dbConnection.ExecuteAsync(sql, model);     // 回傳成功 更新/刪除 的紀錄條數
+        }
+
+        // delete
+        public async Task<int> DeleteProduct(int id)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+            string sql = @"delete from Product
+                           where ProductID = @ProductID";
+            return await dbConnection.ExecuteAsync(sql, id);
+        }
+
+
+
+    }
+}
